65365a4c46d871c89ca8bb67878599aa9609d4fc
CCPA Robotics <132408198+ccpaRobotics@users.noreply.github.com>
2024-04-16 13:28:41 -0700

    changes from scrimmage sat 4/13/2024

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 drive_motor = "6_5169793973449801317"
-servo = "????"
-
-# Set SERVO based on robot info
+arm_motor = "" #arm_motor id 
+servo = "4_1833482520634428295"
+servo_status = 0
+arm_position = 0
 
 def autonomous_setup():
     print ("Autonomous mode has started!")
@@ -23,19 +24,51 @@ def full_speedahead(lv,rv):
     Robot.set_value(drive_motor, "velocity_a", -lv * PWR)
     Robot.set_value(drive_motor, "velocity_b", rv * PWR)
     
+def move_arm(direction):
+    PWR = 10
+    global arm_position
+    Robot.set_value(arm_motor, "velocity_a", direction * PWR)
+    
 def teleop_setup():
+    global servo_status
+    global arm_position
+    arm_position = 0
+    servo_status = 0
+    Robot.set_value(servo, "servo0", 0.9)
     print ("Tele-operated mode has started!")
 
 def teleop_main():
+    global servo_status
+    #servo
+    servo_thing = Robot.get_value(servo,"servo0")
+
+    button_a= Gamepad.get_value("button_a")
+    # servo goes 1 to -1
+    # if you go to 1.0 u will be sad bc not enough voltage
+    if button_a == True and servo_status == 0:
+        print("a is pressed")
+        Robot.set_value(servo, "servo0", 0.9)
+        servo_status = 1
+    
+    if button_a == False and servo_status == 1:
+        print("a is released")
+        Robot.set_value(servo, "servo0", -.9)
+        servo_status = 0
+    
+    #arm motor
+    dpad_up = Gamepad.get_value("dpad_up")
+    dpad_down = Gamepad.get_value("dpad_down")
+    if dpad_up:
+        move_arm(-1)
+    if dpad_down:
+        move_arm(1)
+        #cannot go below 0
+    
+    # drivetrain
     right_y = Gamepad.get_value("joystick_right_y")
     right_x = Gamepad.get_value("joystick_right_x")
-    
-    # this doesn't seem to work
-    Robot.set_value(servo, "servo0", 1)
-    
     if abs(right_y)<0.1 and abs(right_x) < 0.1:
         full_speedahead(0,0)
-
     if right_y <= 0:
         if right_x <= 0:
             #we're in upper left quadrant
@@ -50,12 +83,11 @@ def teleop_main():
         elif right_x >= 0:
             full_speedahead(-right_y,-right_y+right_x)
         
-       # Robot.set_value(motor_id, "velocity_a", -0.5)
-        #Robot.set_value(motor_id, "velocity_b", -0.5)
+        # Robot.set_value(motor_id, "velocity_a", -0.5)
+        # Robot.set_value(motor_id, "velocity_b", -0.5)
+
     else:
         full_speedahead(0,0)
  
         
         #velocity_b connects to Out2 on motor controller
-
-
